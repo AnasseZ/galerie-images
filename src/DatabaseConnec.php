d4ac67faae2e99bbb27b82ca2482cb4d9133533a
Anasse Zougarh <zougarh.anasse@gmail.com>
2017-09-25 15:37:36 +0200

    Ajout des images de la gallery ainsi qu'une carousel permettant d'aller à la suivante ou précédente

--- src/DatabaseConnec.php
+++ src/DatabaseConnec.php
@@ -8,6 +8,7 @@ class DatabaseConnec {
   private $user = 'root';
   private $pass = 'root';
 
+  // La connexion à la base se fait dans le constructeur
   private function __construct()
   {
     try {
@@ -19,6 +20,10 @@ class DatabaseConnec {
     }
   }
 
+/*
+  Methode retournant l'instance actuelle de l'objet selon si elle est déjà
+  existante ou non
+*/
   public static function getInstance()
   {
     if (!self::$instance) {
@@ -33,6 +38,9 @@ class DatabaseConnec {
     return $this->connection;
   }
 
+  /*
+    Préchargement des images dans la base
+  */
   public function loadFixtures()
   {
     $creationRequest = "CREATE TABLE IF NOT EXISTS Pictures (
@@ -45,24 +53,42 @@ class DatabaseConnec {
     $this->connection->query($creationRequest);
 
     $insertQueries = "INSERT INTO Pictures (pic_path, title, description)
-    VALUES ('/assets/bmw.jpg', 'Airbus 300', 'Un avion allant au Mexique.'),
-    ('/assets/car/bmw.jpg', 'BMW 2018', 'La voiture innovante de l\'année.'),
-    ('/assets/bmw.jpg', 'Yamaha X', 'Moto la plus rapide au monde.'),
-    ('/assets/bmw.jpg', 'Decatbic', 'Vélo sans pédale.')
+    VALUES ('/assets/jardin.jpg', 'Airbus 300', 'Un avion allant au Mexique.'),
+    ('/assets/avion.jpg', 'BMW 2018', 'La voiture innovante de l\'année.'),
+    ('/assets/aeroport.jpg', 'Yamaha X', 'Moto la plus rapide au monde.'),
+    ('/assets/maya.jpg', 'Decatbic', 'Vélo sans pédale.')
     ";
 
     $this->connection->query($insertQueries);
   }
 
-  public function affiche()
+  /*
+    Méthode affichant toutes les images de la base via leurs miniatures
+    responsives ainsi que les informations leurs correspondant
+  */
+  public function showMiniatures()
   {
     $selectQuery = "SELECT * FROM Pictures";
     $reponse = $this->connection->query($selectQuery);
 
     while ($donnees = $reponse->fetch()) {
-      echo '<div class="col-md-4"><div class="thumbnail">';
-      echo '<img src="' . $donnees['pic_path'] . '" alt="Lights" style="width:100%">';
+      echo '<div class="col-lg-4 col-md-4 col-xs-6"><div class="thumbnail">';
+      echo '<img src="' . $donnees['pic_path'] . '" alt="' . $donnees['title'] .'" style="width:350px; height:200px" onclick="openModal();currentSlide('. $donnees['id'] .')" class="hover-shadow cursor">';
       echo '<div class="caption"><p><strong>' . $donnees['title'] . '</strong><br>' . $donnees['description'] . '</p></div></div></div>';
     }
   }
+
+  /*
+    Méthode affichant les slides des images dans la Carousel
+  */
+  public function showModalPictures()
+  {
+    $selectQuery = "SELECT * FROM Pictures";
+    $reponse = $this->connection->query($selectQuery);
+
+    while ($donnees = $reponse->fetch()) {
+      echo '<div class="mySlides"><img src="' . $donnees['pic_path'] . '" style="width:100%"></div>';
+    }
+  }
+
 }
--- src/index.php
+++ src/index.php
@@ -4,8 +4,12 @@ session_start();
 
 include("DatabaseConnec.php");
 
+/* Utilisation du pattern singleton afin d'instancier uniquement une seule
+ connexion à la base
+*/
 $instance = DatabaseConnec::getInstance();
 
+// Utilisation des sessions pour ne remplir qu'une seule fois la base au démarrage
 if(!isset($_SESSION['first_run'])) {
     $_SESSION['first_run'] = 1;
     $instance->loadFixtures();
@@ -18,17 +22,39 @@ if(!isset($_SESSION['first_run'])) {
     <title>Projet du mercredi</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="index.css" rel="stylesheet">
+
+    <script src="modal.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
   </head>
 
   <body>
+
+  <div id="myModal" class="modal">
+    <span class="close cursor" onclick="closeModal()">&times;</span>
+    <div class="modal-content">
+      <?php $instance->showModalPictures(); ?>
+      <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+      <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+      <div class="caption-container">
+        <p id="caption"></p>
+      </div>
+
+
+    </div>
+
+
+  </div>
+
     <div class="container">
       <h2> Mini projet </h2>
       <div class="row">
         <?php
-          $instance->affiche();
+          $instance->showMiniatures();
          ?>
       </div>
     </div>
